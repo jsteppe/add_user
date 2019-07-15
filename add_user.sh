65bc2a3cb8ca0762b603e9c4bd9ee9624ffa35d4
Yusif <35215823+jsteppe@users.noreply.github.com>
2019-07-15 16:38:41 +0400

    Added  parameters

--- add_user.sh
+++ add_user.sh
@@ -4,23 +4,23 @@
 echo "Adding user..."
 echo ""
 # GLOBAL VARS
-read -p "Provide a username: " name
+#read -p "Provide a username: " name
 # CHECK AVAILABILITY
-index=$(grep $name $OVPN_FOLDER/openvpn-ca/keys/index.txt)
-avail=$(grep $name /etc/passwd)
+index=$(grep $1 $OVPN_FOLDER/openvpn-ca/keys/index.txt)
+avail=$(grep $1 /etc/passwd)
 OVPN_FOLDER=/home/yusifg
 if [ -z  $avail ] or [ -z $index ]; then
-        adduser $name
+        adduser $1
         echo ""
         echo "User Created!"
         echo ""
         cd $OVPN_FOLDER/openvpn-ca/
         source vars              	# Contains variables of its own, thus executed separately
-        $OVPN_FOLDER/openvpn-ca/build-key $name
-        $OVPN_FOLDER/client-configs/make_config.sh $name
-        cp $OVPN_FOLDER/client-configs/files/$name.ovpn $OVPN_FOLDER/
+        $OVPN_FOLDER/openvpn-ca/build-key $1
+        $OVPN_FOLDER/client-configs/make_config.sh $1
+        cp $OVPN_FOLDER/client-configs/files/$1.ovpn $OVPN_FOLDER/
         exit 0
 else
-        printf "User %s already exists!" $name
+        printf "User %s already exists!" $1
         exit 0
 fi
