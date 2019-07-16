8314ce1a5d041e06e81444ec804e2d05e3cc9bab
Yusif <35215823+jsteppe@users.noreply.github.com>
2019-07-16 18:05:44 +0400

    Added minor features

--- add_user.sh
+++ add_user.sh
@@ -13,6 +13,7 @@ OVPN_FOLDER= # Add a path for the openvpn installation folder
 # CHECK AVAILABILITY
 index=$(grep $1 $OVPN_FOLDER/openvpn-ca/keys/index.txt)
 avail=$(grep $1 /etc/passwd)
+# Check if the vars return zero
 if [ -z  $avail ] or [ -z $index ]; then
         adduser $1
         echo ""
@@ -24,7 +25,16 @@ if [ -z  $avail ] or [ -z $index ]; then
         $OVPN_FOLDER/client-configs/make_config.sh $1
         cp $OVPN_FOLDER/client-configs/files/$1.ovpn $OVPN_FOLDER/
         exit 0
-else
-        printf "User %s already exists!" $1
-        exit 0
+# If the user is already in the system script prompts you to delete it
+elif [ -n $avail ] && [ -n $avail ];then
+        echo "User with this name already exists!"
+        read -p "Do you really want to replace an existing user? [y/n]" answer
+        if [ $answer == y ];then # Thinking about replacing it with "case"
+                userdel -r $1
+                rm -f $OVPN_FOLDER/openvpn-ca/keys/$1.*
+                sed -i "/$1/d" $OVPN_FOLDER/openvpn-ca/keys/index.txt
+                echo "User has been deleted from database!"
+        else
+                exit 0
+        fi
 fi
