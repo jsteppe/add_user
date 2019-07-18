2b98302903c66e16b6c6881f7b56af3186df16c1
Yusif <35215823+jsteppe@users.noreply.github.com>
2019-07-18 17:30:42 +0400

    Small change in functionality

--- add_user.sh
+++ add_user.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 # Adding OpenVPN user to the system and generate a certificate
 # Check if the argument has been passed
+# Check Executing User
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Sorry, you need to run this as root"
+	exit 0
+fi
+
+# Check if BASH is used
+if readlink /proc/$$/exe | grep -q "dash"; then
+	echo "This script needs to be run with bash, not sh"
+	exit 0
+fi
+
+# Check number of arguments
 if [ $# -ne 1 ]; then
         echo "Please, specify a single-word username"
         exit 0
 fi
+
 # Adding user
 echo "Adding user..."
 echo ""
@@ -28,14 +42,15 @@ if [[ -z  $avail or -z $index ]]; then
 # If the user is already in the system script prompts you to delete it
 elif [[ -n $avail && -n $avail ]];then
         echo "User with this name already exists!"
-        read -p "Do you really want to replace an existing user? [y/n]" answer
+        read -n1 -p "Do you really want to replace an existing user? [y/n]" answer
         case $answer in
-                y | Y | "yes" | "YES" | "Yes") userdel -r $1
+                y | Y) userdel -r $1
                    rm -f $OVPN_FOLDER/openvpn-ca/keys/$1.*
                    sed -i "/$1/d" $OVPN_FOLDER/openvpn-ca/keys/index.txt
                    echo "User has been deleted from database!"
                    exit 0
                    ;;
-                *) exit 0
+                N | n) echo
+                       echo OK, goodbye!
         esac
 fi
